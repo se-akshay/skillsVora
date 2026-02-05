df7c64ecae6096d11cd5cf3fa91e5a0917d2201f
se-akshay <akshaysingh6225808@gmail.com>
2026-02-05 16:03:53 +0530

    final commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1]; // Get token from "Bearer <token>"
+    const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({ message: "No token provided" });
